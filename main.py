fa742061eaa66fc5ca2c07e7aad2c67bc975868f
romauov <l2omanov@ya.ru>
2021-03-05 12:27:53 +0400

    some fix

--- main.py
+++ main.py
@@ -2,4 +2,6 @@ from app.app import app
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8888, debug=True)
+    from os import environ
+    port = environ.get("PORT", 8888)
+    app.run(host='0.0.0.0', port=port, debug=True)
